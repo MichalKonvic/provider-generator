15ebbb994230dc5c10aa9addebae7865ce893160
Michal Konvičný <michalkonvic@gmail.com>
2024-04-18 01:32:24 +0200

    SEO & branding

---
+++ src/assets/ReactLogo.tsx
@@ -0,0 +1,21 @@
+import { cn } from '@/lib/utils';
+import React from 'react'
+interface Props {
+  className?: string;
+}
+
+const ReactLogo = ({className}:Props) => {
+  return (
+    <svg className={cn(className)} xmlns="http://www.w3.org/2000/svg" viewBox="-11.5 -10.23174 23 20.46348">
+      <title>React Logo</title>
+      <circle cx="0" cy="0" r="2.05" fill="currentColor"/>
+      <g stroke="currentColor" stroke-width="1" fill="none">
+        <ellipse rx="11" ry="4.2"/>
+        <ellipse rx="11" ry="4.2" transform="rotate(60)"/>
+        <ellipse rx="11" ry="4.2" transform="rotate(120)"/>
+      </g>
+    </svg>
+  )
+}
+
+export default ReactLogo
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,11 +1,19 @@
 "use client";
 
+import ReactLogo from "@/assets/ReactLogo";
 import { ThemeToggle } from "./ThemeToggle";
 
 const Navigation = () => {
   return (
     <div className='sticky top-0 w-full flex items-center justify-between px-2 py-1.5 z-50 border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60'>
-        <p className='text-xl font-bold'>Konvic.</p>
+        <div className="flex items-center">
+          <a href="https://konvic.dev/" className='text-xl font-bold select-none'>Konvic.</a>
+          <p className="text-xl mx-1.5">/</p>
+          <div className="flex items-center gap-1.5">
+            <ReactLogo className="w-4 h-4"/>
+            <p className="">Provider Generator</p>
+          </div>
+        </div>
         <ThemeToggle/>
     </div>
   )
